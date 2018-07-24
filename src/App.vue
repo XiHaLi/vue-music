fbfc8c2c74a22f9bfbaa374ec56a5108a4a8b84c
XiHaLi <13682448009@163.com>
2018-07-24 22:36:34 +0800

    Initialized project

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    hello world
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+  @import "~common/stylus/variable"
+
+  #app
+    color $color-theme
+</style>
